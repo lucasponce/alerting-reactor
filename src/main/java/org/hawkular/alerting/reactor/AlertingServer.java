1850afdbd85c3cf607d258ae7fa7d6022b018bb7
Lucas Ponce <ponce.ballesteros@gmail.com>
2017-04-06 11:45:20 +0200

    Initial commit

---
+++ src/main/java/org/hawkular/alerting/reactor/AlertingServer.java
@@ -0,0 +1,13 @@
+package org.hawkular.alerting.reactor;
+
+/**
+ * Main Alerrting Server
+ *
+ */
+public class AlertingServer
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Alerting Server" );
+    }
+}
